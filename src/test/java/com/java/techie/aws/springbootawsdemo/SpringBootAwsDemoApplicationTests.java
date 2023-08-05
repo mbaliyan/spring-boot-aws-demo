e7716768633c9faba583481cf7571abb47b81115
mbaliyan <84280088+mbaliyan@users.noreply.github.com>
2023-08-06 00:16:33 +0530

    Update SpringBootAwsDemoApplicationTests.java

--- src/test/java/com/java/techie/aws/springbootawsdemo/SpringBootAwsDemoApplicationTests.java
+++ src/test/java/com/java/techie/aws/springbootawsdemo/SpringBootAwsDemoApplicationTests.java
@@ -1,13 +1,13 @@
 package com.java.techie.aws.springbootawsdemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+/*import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;*/
 
-@SpringBootTest
+/*@SpringBootTest
 class SpringBootAwsDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-}
+}*/
